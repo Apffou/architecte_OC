02044531d001cc40a5dcf0ba927e3858d7b209ef
Anaïs Zafiriou-Lejeune <anais.ljn@outlook.fr>
2023-10-19 16:17:37 +0200

    Appel des données, creation du contenu dynamique en JS (foreachcreation contenu html via js), suppression du html devenu inutile

--- Projet_Portfolio_architecte_OC/FrontEnd/assets/script.js
+++ Projet_Portfolio_architecte_OC/FrontEnd/assets/script.js
@@ -1,2 +1,26 @@
-let worksRespons = fetch("http://localhost:5678/api/works");
-console.log(worksRespons)
+// Appel de la ressource 
+const worksRespons = await fetch("http://localhost:5678/api/works");
+const projects = await worksRespons.json();
+console.log(projects)
+
+
+const parentProject = document.querySelector(".gallery");
+
+
+parentProject.innerHTML = "";
+
+projects.forEach(element => {
+
+    const toto = document.createElement("figure")
+    const projectImage = document.createElement("img");
+    const projectLegend = document.createElement("figcaption");
+
+    projectImage.src = element.imageUrl;
+    projectImage.alt = element.title;
+    projectLegend.innerText = element.title;
+
+    toto.appendChild(projectImage)
+    toto.appendChild(projectLegend)
+    parentProject.appendChild(toto)
+    
+});
