8336689352045163d500272ba363ea5c5e1e1186
Anaïs Zafiriou-Lejeune <anais.ljn@outlook.fr>
2023-10-20 12:12:36 +0200

    Filtres bouton actif

--- Projet_Portfolio_architecte_OC/FrontEnd/assets/script.js
+++ Projet_Portfolio_architecte_OC/FrontEnd/assets/script.js
@@ -9,39 +9,73 @@ const galleryDOM = document.querySelector(".gallery");
 // Boucle sur tableau pour creer les elements Projets du site
 projects.forEach(element => {
 
-    const projectParent = document.createElement("figure")
+    const figureDOM = document.createElement("figure")
     const projectImage = document.createElement("img");
     const projectLegend = document.createElement("figcaption");
-
+    figureDOM.setAttribute("id_filters", element.categoryId)
     projectImage.src = element.imageUrl;
     projectImage.alt = element.title;
     projectLegend.innerText = element.title;
 
-    projectParent.appendChild(projectImage);
-    projectParent.appendChild(projectLegend);
-    galleryDOM.appendChild(projectParent);
-    
+
+    figureDOM.appendChild(projectImage);
+    figureDOM.appendChild(projectLegend);
+    galleryDOM.appendChild(figureDOM);
+
 });
 
 // Filtre : utilisation de la propriete category : id.
 // objets html : id 1 / appartements = id2 / hotels & res = id 3 / toutes les categories = Users ID
 
-const categoryRespons =  await fetch("http://localhost:5678/api/categories");
+const categoryRespons = await fetch("http://localhost:5678/api/categories");
 const tagsCategory = await categoryRespons.json();
-console.log(tagsCategory)
 
 
 const parentFilters = document.querySelector(".filters");
-const elementAllFilter = document.createElement("span");
-elementAllFilter.classList.add("tag")
-elementAllFilter.innerText = "Tous";
-parentFilters.appendChild(elementAllFilter);
+
+//Creation bouton span ALL
+let elementFilter = document.createElement("span");
+elementFilter.classList.add("tag")
+elementFilter.innerText = "Tous";
+parentFilters.appendChild(elementFilter);
 
 
+
+// Creation bouton span filtres avec leurs ID
 tagsCategory.forEach(element => {
-    const elementFilter = document.createElement("span");
+    elementFilter = document.createElement("span");
     elementFilter.classList.add("tag");
-    elementFilter.innerText= element.name;
-    elementFilter.setAttribute("id_filters", element.id)
+    elementFilter.innerText = element.name;
+    elementFilter.setAttribute("data-id", element.id)
     parentFilters.appendChild(elementFilter);
+
+
+})
+
+const elementFilterALL = document.querySelectorAll(".filters span");
+
+
+elementFilterALL.forEach(elementfilter => {
+    elementfilter.addEventListener("click", function (event) {
+        let filtreID = event.target.getAttribute("data-id");
+        const figureDomALL = document.querySelectorAll(".gallery figure");
+
+        figureDomALL.forEach(element => {
+            element.style.display = "none";
+        })
+
+        if (filtreID){
+            const filterID1 = document.querySelectorAll(`figure[id_filters='${filtreID}']`);
+
+            filterID1.forEach(elementID1 => {
+                elementID1.style.display = "block";
+            })
+        }else {
+            figureDomALL.forEach(element => {
+                element.style.display = "block";
+            })
+        }
+
+
+    });
 })
