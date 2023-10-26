a2fcb48182a2122a55356b6ad1cf0af5a91f63b6
Anaïs Zafiriou-Lejeune <anais.ljn@outlook.fr>
2023-10-26 17:30:46 +0200

    Ouverture et fermeture de la boite modale

--- Projet_Portfolio_architecte_OC/FrontEnd/script_connected.js
+++ Projet_Portfolio_architecte_OC/FrontEnd/script_connected.js
@@ -1,10 +1,33 @@
-// Cours. Pour la partie pour envoyer du texte, image revoir Types MIME
-const openmodal = function (e) {
+let modal = null;
+
+// Fonction qui permet d'afficher la boite modale
+const openModal = function (e) {
     e.preventDefault();
     const target = document.querySelector(e.target.getAttribute('href'));
+    target.style.display = null;
+    target.removeAttribute('aria-hidden');
+    target.setAttribute('aria-modal', 'true');
+    modal = target;
+    modal.addEventListener('click', closeModal)
+    modal.querySelector('.js-modal-close').addEventListener('click', closeModal)
+}
+// Fonction qui permet de fermer la boite modale
+const closeModal = function (e) {
+    if (modal === null) return
+    e.preventDefault();
+    modal.style.display = "none";
+    modal.setAttribute('aria-hidden', 'true');
+    modal.removeAttribute('aria-modal');
+    modal.removeEventListener('click', closeModal);
+    modal = null;
+    modal.querySelector('.js-modal-close').removeEventListener('click', closeModal)
 }
 
 
 document.querySelectorAll('.js-modal').forEach(a => {
-    a.addEventListener('click', openmodal)
+    a.addEventListener('click', openModal)
 })
+
+
+
+// Cours. Pour la partie pour envoyer du texte, image revoir Types MIME
