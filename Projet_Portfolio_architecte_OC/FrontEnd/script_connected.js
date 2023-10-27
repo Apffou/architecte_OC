a8f72c685fdfbfbccb83f50ab06f1f6f04cbd8f4
Anaïs Zafiriou-Lejeune <anais.ljn@outlook.fr>
2023-10-27 19:07:54 +0200

    Style de la modale preparer en HTML pour faciliter le css.

--- Projet_Portfolio_architecte_OC/FrontEnd/script_connected.js
+++ Projet_Portfolio_architecte_OC/FrontEnd/script_connected.js
@@ -1,8 +1,11 @@
+
+
 let modal = null;
 
 // Fonction qui permet d'afficher la boite modale
 const openModal = function (e) {
     e.preventDefault();
+    console.log("open")
     // On recupère la modale
     const modal = document.querySelector(e.target.getAttribute('href'));
     modal.style.visibility = "visible";
@@ -14,8 +17,9 @@ const openModal = function (e) {
 // Fonction qui permet de fermer la boite modale
 const closeModal = function (e) {
     e.preventDefault();
+    console.log("close")
     let modal = document.querySelector('#modal');
-    modal.style.display = "none";
+    modal.style.visibility = "hidden";
     modal.setAttribute('aria-hidden', 'true');
     modal.removeAttribute('aria-modal');
     e.target.removeEventListener('click', closeModal);
@@ -29,13 +33,36 @@ document.querySelectorAll('.js-modal').forEach(a => {
 
 // Affichage des éléments HTML en mode édition
 
-    const token = window.localStorage.getItem("token");
-    if (token) {
-        const domVisible = document.querySelectorAll(".visible");
-        domVisible.forEach(element => {
-            element.style.visibility = "visible";
-        })
-    }
+const token = window.localStorage.getItem("token");
+if (token) {
+    const domVisible = document.querySelectorAll(".visible");
+    domVisible.forEach(element => {
+        element.style.visibility = "visible";
+    })
+    let modalTitle = document.querySelector(".modal-wrapper h1")
+    modalTitle.innerText = "Galerie photo";
+
+
+
+    // Appel de la ressource 
+    /*
+    const worksRespons = await fetch("http://localhost:5678/api/works");
+    const projects = await worksRespons.json();
+    const galleryDOM = document.querySelector(".js-container-gallery");
+
+    projects.forEach(element => {
+        const divDOM = document.createElement("div");
+        const imageDOM = document.createElement("img");
+        const binDOM = document.createElement("img");
+        imageDOM.src = element.imageUrl;
+
+
+        divDOM.appendChild(imageDOM);
+        divDOM.appendChild(binDOM);
+        galleryDOM.appendChild(divDOM);
+
+    });*/
+}
 
 
 
