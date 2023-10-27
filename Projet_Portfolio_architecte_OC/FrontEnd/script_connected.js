28935b46805176907857d7e279301f705403c8e9
Anaïs Zafiriou-Lejeune <anais.ljn@outlook.fr>
2023-10-27 12:01:31 +0200

    Mise à jour du code HTML et de JS pour afficher les elements avec token sans style dans le html. Ouverture fermeture de la modale simplifié

--- Projet_Portfolio_architecte_OC/FrontEnd/script_connected.js
+++ Projet_Portfolio_architecte_OC/FrontEnd/script_connected.js
@@ -3,30 +3,41 @@ let modal = null;
 // Fonction qui permet d'afficher la boite modale
 const openModal = function (e) {
     e.preventDefault();
-    const target = document.querySelector(e.target.getAttribute('href'));
-    target.style.display = null;
-    target.removeAttribute('aria-hidden');
-    target.setAttribute('aria-modal', 'true');
-    modal = target;
-    modal.addEventListener('click', closeModal)
+    // On recupère la modale
+    const modal = document.querySelector(e.target.getAttribute('href'));
+    modal.style.visibility = "visible";
+    modal.removeAttribute('aria-hidden');
+    modal.setAttribute('aria-modal', 'true');
+    // Click sur croix on ferme
     modal.querySelector('.js-modal-close').addEventListener('click', closeModal)
 }
 // Fonction qui permet de fermer la boite modale
 const closeModal = function (e) {
-    if (modal === null) return
     e.preventDefault();
+    let modal = document.querySelector('#modal');
     modal.style.display = "none";
     modal.setAttribute('aria-hidden', 'true');
     modal.removeAttribute('aria-modal');
-    modal.removeEventListener('click', closeModal);
-    modal = null;
-    modal.querySelector('.js-modal-close').removeEventListener('click', closeModal)
+    e.target.removeEventListener('click', closeModal);
 }
 
 
 document.querySelectorAll('.js-modal').forEach(a => {
     a.addEventListener('click', openModal)
 })
+
+
+// Affichage des éléments HTML en mode édition
+
+    const token = window.localStorage.getItem("token");
+    if (token) {
+        const domVisible = document.querySelectorAll(".visible");
+        domVisible.forEach(element => {
+            element.style.visibility = "visible";
+        })
+    }
+
+
 
 
 
